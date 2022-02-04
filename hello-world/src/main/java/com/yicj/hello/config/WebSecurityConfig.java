3a5cf9ff72f5c5b869223871c681fa12b54ed738
易成杰 <626659321@qq.com>
2022-02-04 14:10:40 +0800

    save

---
+++ hello-world/src/main/java/com/yicj/hello/config/WebSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.yicj.hello.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class WebSecurityConfig {
+
+}
