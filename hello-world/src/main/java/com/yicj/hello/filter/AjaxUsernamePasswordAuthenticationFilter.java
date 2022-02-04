116132a068752badb8ec4d8f38decf48645dd43a
yichengjie <626659321@qq.com>
2022-02-04 16:21:31 +0800

    save

---
+++ hello-world/src/main/java/com/yicj/hello/filter/AjaxUsernamePasswordAuthenticationFilter.java
@@ -0,0 +1,24 @@
+package com.yicj.hello.filter;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AjaxUsernamePasswordAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+    protected AjaxUsernamePasswordAuthenticationFilter() {
+        super(new AntPathRequestMatcher("/login", "POST"));
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        return null;
+    }
+}
