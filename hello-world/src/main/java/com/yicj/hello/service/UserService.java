039db709a06ba0b22ff04224fc2cc251c56bac94
易成杰 <626659321@qq.com>
2022-02-05 19:58:30 +0800

    save

--- hello-world/src/main/java/com/yicj/hello/service/UserService.java
+++ hello-world/src/main/java/com/yicj/hello/service/UserService.java
@@ -4,7 +4,12 @@ import com.yicj.hello.jpa.entity.UserDO;
 import com.yicj.hello.mvc.dto.UserDTO;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.List;
+import java.util.concurrent.Future;
+
 public interface UserService extends UserDetailsService {
 
     UserDO saveUser(UserDTO userDTO) ;
+
+    Future<List<UserDO>> getAllUserAsync() ;
 }
--- hello-world/src/main/java/com/yicj/hello/service/impl/UserServiceImpl.java
+++ hello-world/src/main/java/com/yicj/hello/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.yicj.hello.jpa.entity.UserDO;
 import com.yicj.hello.mvc.dto.UserDTO;
 import com.yicj.hello.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
@@ -12,7 +13,10 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 
 
 @Service
@@ -31,5 +35,29 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDO saveUser(UserDTO userDTO) {
         return null;
+    }
+
+    @Override
+    public Future<List<UserDO>> getAllUserAsync() {
+        // AsyncResult
+        return new AsyncResult<>(getAllUser());
+    }
+
+    public List<UserDO> getAllUser(){
+
+        UserDO u1 = new UserDO() ;
+        u1.setUsername("username1");
+        //
+        UserDO u2 = new UserDO() ;
+        u2.setUsername("username1");
+        //
+        UserDO u3 = new UserDO() ;
+        u3.setUsername("username1");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return Arrays.asList(u1,u2,u3) ;
     }
 }
