701161dcf653e32762fd0225aeb1e6e06e725411
yichengjie <626659321@qq.com>
2022-06-11 09:32:21 +0800

    保存

---
+++ hello-security/src/main/java/com/yicj/security/HelloSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.yicj.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSecurityApplication.class, args) ;
+    }
+}
