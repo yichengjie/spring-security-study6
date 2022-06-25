4f6126794f6692db3a50a681d1aa862807e7b570
yichengjie <626659321@qq.com>
2022-06-25 15:35:33 +0800

    save

--- hello-code/src/main/java/com/yicj/code/config/AppConfig.java
+++ hello-code/src/main/java/com/yicj/code/config/AppConfig.java
@@ -1,12 +1,33 @@
 package com.yicj.code.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class AppConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private AuthenticationProvider authenticationProvider ;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll() ;
+                //.and()
+                //.exceptionHandling().authenticationEntryPoint()
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+        //auth.authenticationProvider(authenticationProvider) ;
+    }
 }
---
+++ hello-code/src/main/java/com/yicj/code/provider/MyAuthenticationProvider.java
@@ -0,0 +1,20 @@
+package com.yicj.code.provider;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
+}
