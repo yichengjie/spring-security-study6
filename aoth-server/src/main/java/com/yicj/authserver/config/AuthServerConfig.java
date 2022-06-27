8db494b85afd2839534348e82b42007d36c458c1
yichengjie <626659321@qq.com>
2022-06-27 21:34:37 +0800

    save

--- aoth-server/src/main/java/com/yicj/authserver/config/AuthServerConfig.java
+++ aoth-server/src/main/java/com/yicj/authserver/config/AuthServerConfig.java
@@ -3,10 +3,14 @@ package com.yicj.authserver.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import javax.sql.DataSource;
@@ -16,15 +20,30 @@ import javax.sql.DataSource;
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
-    private DataSource dataSource ;
+    private AuthenticationManager authenticationManager ;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder ;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("client-id")
+                .secret(passwordEncoder.encode("client-secret"))
+                .scopes("read", "write")
+                .authorizedGrantTypes("password", "refresh_token")
+                .authorities("user:view");
+    }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager) ;
         endpoints.tokenStore(tokenStore());
     }
 
     @Bean
     public TokenStore tokenStore(){
-        return new JdbcTokenStore(dataSource) ;
+        TokenStore tokenStore = new InMemoryTokenStore() ;
+        return tokenStore ;
     }
 }
---
+++ aoth-server/src/main/java/com/yicj/authserver/service/MyUserDetailService.java
@@ -0,0 +1,21 @@
+package com.yicj.authserver.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class MyUserDetailService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority("ROLE_ADMIN") ;
+        return new User(username, "123456", Arrays.asList(grantedAuthority));
+    }
+}
