6f3bf5f0b8ef3b03cca8940d5ffadc58b679d8fc
yichengjie <626659321@qq.com>
2022-06-30 22:16:05 +0800

    save

--- aoth-server/src/main/java/com/yicj/authserver/config/AuthServerConfig.java
+++ aoth-server/src/main/java/com/yicj/authserver/config/AuthServerConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -19,6 +22,12 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
     private PasswordEncoder passwordEncoder ;
+
+    @Autowired
+    private TokenStore tokenStore ;
+
+    @Autowired
+    private JwtAccessTokenConverter jwtAccessTokenConverter ;
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -46,6 +55,8 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager) ;
+        endpoints.authenticationManager(authenticationManager)
+                .tokenStore(tokenStore)
+                .accessTokenConverter(jwtAccessTokenConverter);
     }
 }
---
+++ aoth-server/src/main/java/com/yicj/authserver/config/TokenConfig.java
@@ -0,0 +1,30 @@
+package com.yicj.authserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class TokenConfig {
+
+    private String SIGNING_KEY = "oauth2-Panghl$0";
+
+    // 令牌存储策略
+    @Bean
+    public TokenStore tokenStore(JwtAccessTokenConverter jwtAccessTokenConverter) {
+        //JWT令牌存储方案
+        return new JwtTokenStore(jwtAccessTokenConverter);
+    }
+    /***
+     * 定义JJwtAccessTokenConverter
+     * @return
+     */
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(SIGNING_KEY); //对称秘钥，资源服务器使用该秘钥来验证
+        return converter;
+    }
+}
